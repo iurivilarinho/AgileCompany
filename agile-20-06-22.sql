9ace69919202b8b0b426802862555578c8cfe81c
iuri Vilarinho <iuri-vilarinho@hotmail.com>
2022-06-24 18:43:58 -0300

    Agile

---
+++ agile-20-06-22.sql
@@ -0,0 +1,162 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
+USE `mydb` ;
+
+-- -----------------------------------------------------
+-- Table `mydb`.`cliente`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`cliente` (
+  `idcliente` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(100) NOT NULL,
+  `cpf` VARCHAR(11) NOT NULL,
+  `telefone` VARCHAR(10) NOT NULL,
+  `sexo` VARCHAR(1) NOT NULL,
+  `email` VARCHAR(100) NOT NULL,
+  `usuario` VARCHAR(45) NOT NULL,
+  `senha` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`idcliente`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`veiculo`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`veiculo` (
+  `id_veiculo` INT NOT NULL AUTO_INCREMENT,
+  `veiculo` VARCHAR(45) NOT NULL,
+  `marca` VARCHAR(45) NOT NULL,
+  `ano` VARCHAR(45) NOT NULL,
+  `placa` VARCHAR(45) NOT NULL,
+  `modelo` VARCHAR(45) NOT NULL,
+  `cliente_idcliente` INT NOT NULL,
+  `imagem` LONGTEXT NULL,
+  INDEX `fk_veiculo_cliente1_idx` (`cliente_idcliente` ASC) ,
+  PRIMARY KEY (`id_veiculo`),
+  CONSTRAINT `fk_veiculo_cliente1`
+    FOREIGN KEY (`cliente_idcliente`)
+    REFERENCES `mydb`.`cliente` (`idcliente`)
+    ON DELETE CASCADE
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`viagem`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`viagem` (
+  `id_viagem` INT NOT NULL AUTO_INCREMENT,
+  `viagem_idcliente` INT NOT NULL,
+  `horario` VARCHAR(45) NOT NULL,
+  `data` VARCHAR(45) NOT NULL,
+  `peso` VARCHAR(45) NOT NULL,
+  `espaco` VARCHAR(45) NOT NULL,
+  `kg_max` VARCHAR(45) NOT NULL,
+  `valo_km` VARCHAR(45) NOT NULL,
+  `distancia_perc` VARCHAR(45) NOT NULL,
+  `veiculo_id_veiculo` INT NOT NULL,
+  PRIMARY KEY (`id_viagem`),
+  INDEX `fk_viagem_veiculo1_idx` (`veiculo_id_veiculo` ASC) ,
+  CONSTRAINT `fk_viagem_veiculo1`
+    FOREIGN KEY (`veiculo_id_veiculo`)
+    REFERENCES `mydb`.`veiculo` (`id_veiculo`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`origem_carga`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`origem_carga` (
+  `id_origem_carga` INT NOT NULL AUTO_INCREMENT,
+  `viagem_id` INT NOT NULL,
+  `cep` VARCHAR(10) NOT NULL,
+  `uf` VARCHAR(2) NOT NULL,
+  `cidade` VARCHAR(50) NOT NULL,
+  `bairro` VARCHAR(45) NOT NULL,
+  `rua` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id_origem_carga`),
+  CONSTRAINT `fk_viagem_origem`
+    FOREIGN KEY (`viagem_id`)
+    REFERENCES `mydb`.`viagem` (`id_viagem`)
+    ON DELETE CASCADE
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`end_cliente`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`end_cliente` (
+  `id_end_cliente` INT NOT NULL AUTO_INCREMENT,
+  `cidade` VARCHAR(50) NOT NULL,
+  `uf` VARCHAR(2) NOT NULL,
+  `cep` VARCHAR(10) NOT NULL,
+  `bairro` VARCHAR(45) NOT NULL,
+  `numero` VARCHAR(6) NOT NULL,
+  `rua` VARCHAR(45) NOT NULL,
+  `cliente_id` INT NOT NULL,
+  INDEX `fk_end_cliente_cliente_idx` (`cliente_id` ASC) ,
+  PRIMARY KEY (`id_end_cliente`),
+  CONSTRAINT `fk_end_cliente_cliente`
+    FOREIGN KEY (`cliente_id`)
+    REFERENCES `mydb`.`cliente` (`idcliente`)
+    ON DELETE CASCADE
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`destino_carga`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`destino_carga` (
+  `id_destino_carga` INT NOT NULL AUTO_INCREMENT,
+  `viagem_id` INT NOT NULL,
+  `cep` VARCHAR(10) NOT NULL,
+  `uf` VARCHAR(2) NOT NULL,
+  `cidade` VARCHAR(50) NOT NULL,
+  `bairro` VARCHAR(45) NOT NULL,
+  `rua` VARCHAR(45) NOT NULL,
+  `numero` VARCHAR(6) NOT NULL,
+  INDEX `fk_destino_carga_viagem1_idx` (`viagem_id` ASC) ,
+  PRIMARY KEY (`id_destino_carga`),
+  CONSTRAINT `fk_destino_carga_viagem1`
+    FOREIGN KEY (`viagem_id`)
+    REFERENCES `mydb`.`viagem` (`id_viagem`)
+    ON DELETE CASCADE
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`frete`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`frete` (
+  `id_frete` INT NOT NULL AUTO_INCREMENT,
+  `valor_total` FLOAT NOT NULL,
+  `viagem_id` INT NOT NULL,
+  INDEX `fk_frete_viagem1_idx` (`viagem_id` ASC) ,
+  PRIMARY KEY (`id_frete`),
+  CONSTRAINT `fk_frete_viagem1`
+    FOREIGN KEY (`viagem_id`)
+    REFERENCES `mydb`.`viagem` (`id_viagem`)
+    ON DELETE CASCADE
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
---
+++ index.js
@@ -0,0 +1,376 @@
+const express = require('express') //IMPORTANDO A FRAMEORK EXPRESS
+const bodyparser = require('body-parser') //IMPORTANDO O BODY-PARSER
+const app = express() //ATRIBUINDO O EXPRESS A VARIAVEL APP
+const port = 5000; //DEFININDO EM QUAL PORTA IRA RODAR O SERVIDOR
+const axios = require('axios');
+//const { query } = require('express');
+
+
+
+
+app.use(bodyparser.urlencoded({extended: false}))
+app.use(bodyparser.json()) //TRANSFORMA OS DADOS DO BODY EM JSON
+app.use(express.json()); //APLICANDO EXPRESS NO PROJETO
+
+// CONEXÃO COM BANCO DE DADOS
+async function conecta() {
+  if ( global.conexao && global.conexao.state !== 'disconnected' ) {
+    return global.conexao;
+  }
+
+  const mysql = require('mysql2/promise'); //IMPORTANDO MYSQL2 PARA O PROJETO
+  const conexao = await mysql.createConnection('mysql://root:@localhost:3306/mydb'); //DEFININDO ROTA DO BANCO DE DADOS
+  console.log('Mysql conectado.');
+
+  global.conexao = conexao;
+  return conexao;
+}
+
+  
+app.post('/login', async (req, res)=>{
+    const conn = await conecta(); //CONECTANDO AO BANDO DE DADOS
+    const {email, senha} = req.body; // RECEBENDO INFORMAÇÕES DA TELA DE LOGIN
+
+    //SELECIONANDO TABELA CLIENTE E VERIFICANDO SE EXISTE EMAIL E SENHA INFORMADOS E ATRIBUINDO DADOS DO USUARIO A VARIAVEL SESSION CASO A CONSULTA ENCONTRE RESULTADOS
+    var [session] = await conn.query(`SELECT * FROM cliente WHERE email = '${email}' and senha = '${senha}' `)
+  
+  
+//PERCORRENDO O VETOR SESSION E ATRIBUINDO SEUS DADOS A VARIAVEL SESSAO
+  for(let i = 0; i < session.length; i++) {
+
+    if(session[i].email == email  && session[i].senha == senha){
+     
+       sessao = session[i]
+    
+       //ENVIANDO DADOS DO USUARIO PARA A ROTA LOGIN
+       app.get('/login', async (req,res)=>{
+        return res.json( sessao)
+
+       })
+      
+    } return res.json({message:"ok"})
+
+  } 
+
+})
+
+//ENCONTRA E ARMAZENA OS DADOS ENVIADOS A ROTA GET LOGIN
+async function logado(){
+  try {
+
+    //ATRIBUINDO VALORES DA ROTA A VARIAVEL DATA
+    const {data} = await axios('http://localhost:5000/login');
+    return data;
+        
+  } catch (error) {
+      console.log('erro')
+    }
+
+}
+
+// TESTE DE CONEXÃO SERVIDOR
+app.get('/', async function (req, res) {
+    const conn = conecta(); //CONECTANDO AO BANDO DE DADOS A ROTA TESTE
+    res.send('servidor rodando');
+  
+});
+
+
+// CONSULTA TODOS OS USUARIOS
+app.get('/user', async function (req, res) {
+    const conn = await conecta();//CONECTANDO AO BANDO DE DADOS
+
+  //SELECIONANDO TODOS OS CLIENTES 
+    const [cliente] = await conn.query(`SELECT * FROM cliente `)
+
+    //ENVIANDO DADOS DOS CLIENTES EM JSON PARA A ROTA GET USER
+    return res.json( cliente)
+    
+});
+
+// CONSULTA USUARIOS POR ID
+app.get('/user/:id',async (req, res)=>{
+    const {id} = req.params; 
+    const conn  = await conecta(); //CONECTANDO AO BANDO DE DADOS
+    //SELECIONA CLIENTE REFERENTE AO ID INFORMADO
+    const [cliente] = await conn.query(`SELECT * FROM cliente WHERE idcliente = '${id}'`)
+    //ENVIANDO DADOS DO CLIENTE EM JSON PARA A ROTA GET USER/ID
+    return res.json( cliente)
+
+})
+// CADASTRA UM USUARIO
+app.post('/user/cadastro' , async (req, res)=> {
+  
+    const conn = await conecta();//CONECTANDO AO BANDO DE DADOS
+
+
+    //RECEBENDO VALORES INFORMAS NO BODY(FRONTEND) REFERENCIADOS COM NAME PARA SEREM RECEBIDOS COM req.body.campoDesejao
+    const {nome, cpf, telefone, sexo, email, senha, usuario, numero, cidade, uf, cep, rua, bairro } = req.body
+
+    //INSERE DADOS NA TABELA CLIENTE
+    const sql = `INSERT INTO cliente(nome, cpf, telefone, sexo, email, usuario, senha) VALUES('${nome}','${cpf}','${telefone}','${sexo}','${email}','${usuario}','${senha}')`;
+    
+   
+    //GRAVA OS DADOS NA RESPECTIVAS TABELAS CLIENTE E CLIENTE_END
+    return await conn.query(sql) && res.json({message:"CADASTRO EFETUADO"})            
+    
+});
+
+//CADASTRA O ENDEREÇO DO USUARIO
+app.post('/user/cadastro/endereco' , async (req, res)=> {
+  
+  const conn = await conecta();//CONECTANDO AO BANDO DE DADOS
+  const sessao = await logado();
+  const id = sessao.idcliente;
+
+  //RECEBENDO VALORES INFORMAS NO BODY(FRONTEND) REFERENCIADOS COM NAME PARA SEREM RECEBIDOS COM req.body.campoDesejao
+  const {numero, cidade, uf, cep, rua, bairro } = req.body
+
+//INSERE DADOS NA TABELA CLIENTE _END
+  const sql = `INSERT INTO end_cliente(cidade,uf, cep, bairro, numero, rua, cliente_id) VALUES('${cidade}','${uf}','${cep}','${bairro}','${numero}','${rua}', '${id}')`;
+  
+  //GRAVA OS DADOS NA TABELA CLIENTE_END
+  return await conn.query(sql) && res.json({message:"CADASTRO EFETUADO"})            
+  
+});
+
+// ALTERA UM USUARIO
+app.post('/user/:id' , async (req, res)=> {
+    const {id} = req.params
+    const conn = await conecta(); //CONECTANDO AO BANDO DE DADOS
+
+    //RECEBENDO VALORES INFORMAS NO BODY(FRONTEND) REFERENCIADOS COM NAME PARA SEREM RECEBIDOS COM req.body.campoDesejao
+    const {nome, cpf, telefone, sexo, email, senha, usuario, numero, cidade, uf, cep, rua, bairro } = req.body
+    //SELECIONA  TABELA CLIENTE PARA ATUALIZAR USUARIO
+    const sql = `UPDATE cliente SET nome = '${nome}', cpf ='${cpf}', telefone = '${telefone}', sexo = '${sexo}', email = '${email}', usuario = '${usuario}', senha = '${senha}' WHERE idcliente = '${id}'`;
+
+    //SELECIONA  TABELA END_CLIENTE PARA ATUALIZAR ENDEREÇO DE USUARIO SELECIONADO
+    const sql2 = `UPDATE end_cliente SET cidade = '${cidade}', uf = '${uf}', cep = '${cep}', bairro = '${bairro}', numero = '${numero}', rua = '${rua}' WHERE cliente_idcliente = '${id}'`;
+            
+    return await conn.query(sql) && conn.query(sql2) && res.json({message:"Alteração bem suscessedida"})
+
+
+});
+
+// DELETA UM USUARIO
+app.delete('/user/:id' , async (req, res)=> {
+    const {id} = req.params
+    const conn = await conecta(); //CONECTANDO AO BANDO DE DADOS
+
+    //SELECIONA USUARIO A SER DELETADO
+    const sql = `DELETE FROM cliente WHERE idcliente = '${id}'`;
+    //EXECUTA O DELETE NA TABELA CLIENTE
+    return await conn.query(sql) && res.json({mensagem: "Cliente excluido com sucesso" })
+    
+
+});
+
+//METODOS VEICULO____________________________________________________________________________________
+
+//CONSULTA VEICULO DO USUARIO LOCAL
+app.get('/user/veiculo/consulta', async function (req, res) {
+    const conn = await conecta();//CONECTANDO AO BANDO DE DADOS
+    const sessao = await logado();//IMPORTANDO DADOS DO USUARIO LOGADO
+    const id = sessao.idcliente;//ATRIBUINDO O ID DO USUARIO LOGADO A VARIAVEL ID
+
+    //SELECIONA OS VEICULOS CORRESPONDENTES AO USUARIO LOGADO
+    const [veiculo] = await conn.query(`SELECT * FROM veiculo where cliente_idcliente = '${id}' `)
+    //ENVIA OS DADOS PARA A ROTA GET USER/VEICULO/CONSULTA
+    return res.json( veiculo)
+    
+});
+// CADASTRA UM VEICULO
+app.post('/user/veiculo/cadastro' , async (req, res)=> {
+    const conn = await conecta(); //CONECTANDO AO BANDO DE DADOS
+    const sessao = await logado(); //IMPORTANDO DADOS DO USUARIO LOGADO
+    const id = sessao.idcliente; //ATRIBUINDO O ID DO USUARIO LOGADO A VARIAVEL ID
+
+    //RECEBENDO VALORES INFORMAS NO BODY(FRONTEND) REFERENCIADOS COM NAME PARA SEREM RECEBIDOS COM req.body.campoDesejado
+    const {veiculo, marca, ano, placa, modelo, imagem} = req.body
+    
+    //SELECIONA  TABELA VEICULO PARA INSERÇÃO DE NOVO VEICULO DO USUARIO
+    const sql = `INSERT INTO veiculo(veiculo, marca, ano, placa, modelo, imagem, cliente_idcliente) VALUES('${veiculo}','${marca}','${ano}','${placa}','${modelo}','${imagem}','${id}')`;
+    //EXECUTA O UPDATE NA TABELA VEICULO
+    return await conn.query(sql) && res.json({mensagem: "Cadastro de Veiculo Efetuado!" });
+            
+
+});
+
+//ALTERA UM VEICULO REFERENTE AO USUARIO 
+app.put('/user/veiculo/atualiza' , async (req, res)=> {
+    const conn = await conecta(); //CONECTANDO AO BANDO DE DADOS
+    const sessao = await logado(); //IMPORTANDO DADOS DO USUARIO LOGADO
+    const id = sessao.idcliente; //ATRIBUINDO O ID DO USUARIO LOGADO A VARIAVEL ID
+
+    //RECEBENDO VALORES INFORMAS NO BODY(FRONTEND) REFERENCIADOS COM NAME PARA SEREM RECEBIDOS COM req.body.campoDesejao
+    const {veiculo, marca, ano, placa, modelo, imagem} = req.body
+
+    //SELECIONA  TABELA VEICULO PARA ALTERAÇÃO DO VEICULO DO USUARIO
+    const sql = `UPDATE veiculo SET veiculo = '${veiculo}', marca = '${marca}', ano = '${ano}', placa = '${placa}', modelo = '${modelo}', imagem = '${imagem}' where cliente_idcliente = '${id}'`;
+    //EXECUTA A ALTERAÇÃO NA TABELA VEICULO
+    return await conn.query (sql) && res.json({mensagem: "Ateração concluida" });
+
+});
+
+//DELETA UM VEICULO REFERENTE AO USUARIO CADASTRADOe E ID DO VEICULO SELECIONADO
+app.delete('/user/veiculo/deletar' , async (req, res)=> {
+    const conn = await conecta(); //CONECTANDO AO BANDO DE DADOS
+    const sessao = await logado(); //IMPORTANDO DADOS DO USUARIO LOGADO
+    const id = sessao.idcliente; //ATRIBUINDO O ID DO USUARIO LOGADO A VARIAVEL ID
+
+    const {veiculo} = req.body
+    //SELECIONA USUARIO A SER DELETADO
+    const sql = `DELETE FROM veiculo WHERE cliente_idcliente = '${id}' and id_veiculo = ${veiculo}`;
+    //EXECUTA O DELETE NA TABELA VEICULO
+    return await conn.query(sql) && res.json({mensagem: "veiculo excluido com sucesso" });
+
+});
+
+
+// INFORMA EM QUAL PORTA ESTA RODANDO O SERVIDOR NO CONSOLE DO VISUAL STUDIO
+app.listen(port, () => {
+    console.log(`Exemplo app node rodando no endereço http://localhost:${port}`);
+});
+
+//ROTAS VIAGEM ____________________________________________________________________________________________________
+
+//ENVIA OS VEICULOS PARA A TELA DE CADASTRO PARA O USUARIO SELECIONAR
+app.get('/viagem/cadastro', async (req,res)=>{
+
+  conn = await conecta()
+  sessao = await logado()
+  const id = sessao.idcliente;
+  const transport = await conn.query(`SELECT * FROM veiculo WHERE cliente_idcliente = '${id}'`)
+  
+  
+  //PERCORRENDO O VETOR TRASNPORT E ATRIBUINDO SEUS DADOS A VARIAVEL SESSAO
+    for(let i = 0; i < transport.length; i++) {
+       
+        transport2 = transport[i]
+        return res.json(transport2)
+        
+      }
+    
+})
+
+
+//listar viagem
+app.get('/viagem/exibir', async (req,res)=>{
+
+  conn = await conecta()
+  sessao = await logado()
+  const id = sessao.idcliente;
+
+  const [viagem] = await conn.query(`SELECT *FROM viagem where viagem_idcliente = '${id}'`);
+  
+
+  for(let i = 0; i < viagem.length; i++) {
+     
+       viagem2 = await viagem[i]
+       
+  
+       const [exibirViagem] = await conn.query(`SELECT 
+       viagem.horario, viagem.data, viagem.peso, viagem.espaco, viagem.kg_max, viagem.valo_km, 
+       veiculo.veiculo, veiculo.marca, veiculo.imagem, veiculo.ano,
+       origem_carga.cep, origem_carga.uf, origem_carga.cidade, origem_carga.bairro,
+       destino_carga.cep, destino_carga.uf, destino_carga.cidade, destino_carga.bairro, destino_carga.rua, destino_carga.numero,
+       cliente.nome, cliente.telefone, cliente.email
+     
+       FROM viagem INNER JOIN veiculo
+       ON '${viagem2.veiculo_id_veiculo}' = veiculo.id_veiculo 
+       INNER JOIN origem_carga
+       ON '${viagem2.id_viagem}' = origem_carga.viagem_id 
+       INNER JOIN destino_carga
+       ON '${viagem2.id_viagem}' = destino_carga.viagem_id
+       INNER JOIN cliente
+       ON '${viagem2.viagem_idcliente}' = cliente.idcliente`)
+
+       for(let j = 0; j < exibirViagem.length; j++) {
+     
+        exibirViagem2 = await exibirViagem[j]
+
+       console.log(exibirViagem2)
+   //GRAVA OS DADOS NA RESPECTIVAS TABELAS DESTINO E ORIGEM
+   return await res.json(exibirViagem2)  
+       }
+  }
+  
+  
+})
+
+
+//CADASTRA UMA VIAGEM
+app.post('/viagem/cadastro' , async (req, res)=> {
+  
+  const conn = await conecta();//CONECTANDO AO BANDO DE DADOS
+  const sessao = await logado(); //IMPORTANDO DADOS DO USUARIO LOGADO
+  const id = sessao.idcliente; //ATRIBUINDO O ID DO USUARIO LOGADO A VARIAVEL ID
+
+    //MODELO PARA CADASTRAR VEICULO SELECIONADO
+
+    // const veiculo_selecionado = req.body.veiculo_selecionado;
+
+    // const [veic_select] = await conn.query(`SELECT * FROM VEICULO id_veiculo where veiculo = '${veiculo_selecionado}'`) 
+
+    //   for(let i = 0; i < veic_select.length; i++) {
+       
+    //     veic_select2 = veic_select[i]
+      
+    // }
+
+  //RECEBENDO VALORES INFORMAS NO BODY(FRONTEND) REFERENCIADOS COM NAME PARA SEREM RECEBIDOS COM req.body.campoDesejao
+  const {horario_partida, data_partida, kg_max, espaco, km_percorrido,valo_km } = req.body //tabela viagem
+
+  const veic_select2 = 1;
+
+  //INSERE DADOS NA TABELA VIAGEM
+  const sql = `INSERT INTO viagem(viagem_idcliente, horario, data, espaco, kg_max, valo_km, distancia_perc, veiculo_id_veiculo) VALUES('${id}','${horario_partida}','${data_partida}','${espaco}','${kg_max}','${valo_km}','${km_percorrido}','${veic_select2}')`;
+
+  //GRAVA OS DADOS NA RESPECTIVAS TABELAS CLIENTE E CLIENTE_END
+  return await conn.query(sql) && res.json({message:"CADASTRO EFETUADO"})            
+
+});
+
+//CADASTRA ORIGEM E DESTINO DA VIAGEM
+app.post('/viagem/cadastro/rota' , async (req, res)=> {
+  
+  const conn = await conecta();//CONECTANDO AO BANDO DE DADOS
+  const sessao = await logado(); //IMPORTANDO DADOS DO USUARIO LOGADO
+  const id = sessao.idcliente; //ATRIBUINDO O ID DO USUARIO LOGADO A VARIAVEL ID
+
+  //RECEBENDO VALORES INFORMAS NO BODY(FRONTEND) REFERENCIADOS COM NAME PARA SEREM RECEBIDOS COM req.body.campoDesejao
+  const {cep_partida, uf_partida, cidade_partida, bairro_partida, rua_partida} = req.body //tabela origem
+  const {cep_destino, uf_destino, cidade_destino, bairro_destino, rua_destino, numero} = req.body // tabela destino
+
+  const [viagem] = await conn.query(`SELECT id_viagem FROM viagem where viagem_idcliente = '${id}' ORDER BY id_viagem DESC LIMIT 1`);
+  
+
+  for(let i = 0; i < viagem.length; i++) {
+     
+       viagem2 = await viagem[i]
+       
+       //INSERE DADOS NA TABELA ORIGEM
+   const sql = `INSERT INTO origem_carga(viagem_id, cep, uf, cidade, bairro, rua) VALUES('${viagem2.id_viagem}','${cep_partida}','${uf_partida}','${cidade_partida}','${bairro_partida}','${rua_partida}')`;
+
+  //INSERE DADOS NA TABELA DESTINO
+   const sql2 = `INSERT INTO destino_carga(viagem_id, cep, uf, cidade, bairro, rua, numero) VALUES('${viagem2.id_viagem}','${cep_destino}','${uf_destino}','${cidade_destino}','${bairro_destino}','${rua_destino}', '${numero}')`;
+
+   //GRAVA OS DADOS NA RESPECTIVAS TABELAS DESTINO E ORIGEM
+   return await conn.query(sql) && conn.query(sql2) && res.json({message:"CADASTRO DE ROTAS EFETUADO"})  
+       
+  }
+});
+
+app.delete('/user/viagem/deletar' , async (req, res)=> {
+  const conn = await conecta(); //CONECTANDO AO BANDO DE DADOS
+  const sessao = await logado(); //IMPORTANDO DADOS DO USUARIO LOGADO
+  const id = sessao.idcliente; //ATRIBUINDO O ID DO USUARIO LOGADO A VARIAVEL ID
+
+  const {viagem} = req.body
+  //SELECIONA VIAGEM A SER DELETADA
+  const sql = `DELETE FROM viagem WHERE viagem_idcliente = '${id}' and id_viagem = ${viagem}`;
+  //EXECUTA O DELETE NA TABELA VIAGEM
+  return await conn.query(sql) && res.json({mensagem: "viagem excluida com sucesso" });
+
+});
+
